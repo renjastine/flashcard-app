1f352c9028e86fa322174cb2fcd35811be0bd9c6
ren jastine <rnjstntmj@gmail.com>
2025-11-15 23:08:05 +0800

    Flashcard design done

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
+import Header from "./components/Header";
+import NumOfCards from "./components/NumOfCards";
+import Controls from "./components/Controls";
+import Flashcards from "./components/Flashcards";
 
 export default function Home() {
   return (
     <div className="flex justify-center w-screen">
-      <main className="h-96 flex justify-center items-center p-2 w-full max-w-[730px]">
-        <div className="flex justify-between items-center w-full">
-          <h1 className="">Flashcard Quiz</h1>
-          <button className="flex items-center border px-3 gap-2 h-9 bg-stone-900 text-stone-100 rounded-md">
-            <img className="w-4" src="/add-white.svg" alt="add" />
-            Manage Cards
-          </button>
-        </div>
+      <main className="flex flex-col justify-center items-center p-4 w-full max-w-[730px] gap-6">
+        <Header />
+        <NumOfCards />
+        <Flashcards />
+        <Controls />
       </main>
     </div>
   );

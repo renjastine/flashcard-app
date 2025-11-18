c84a8e7a96c2cf01caee3ea5088eb5c10b99a4cf
ren jastine <rnjstntmj@gmail.com>
2025-11-18 22:01:09 +0800

    Remove unnecessary components

--- app/components/NumOfCards.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NumOfCards = () => {
-  return (
-    <h1 className="text-stone-400">Card 1 of 3</h1>
-  )
-}
-
-export default NumOfCards
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Header from "./components/Header";
-import NumOfCards from "./components/NumOfCards";
-import Controls from "./components/Controls";
 import Flashcards from "./components/Flashcards";
 import ManageCardsModal from "./components/ManageCardsModal";
 
@@ -8,10 +5,25 @@ export default function Home() {
   return (
     <div className="flex justify-center w-screen h-screen">
       <main className="flex flex-col justify-center items-center p-4 w-full max-w-[730px] gap-6">
-        <Header />
-        <NumOfCards />
+        < div className="flex justify-between items-center w-full" >
+          < h1 className="" > Flashcard Quiz</h1 >
+          < button className="flex items-center border px-3 gap-2 h-9 bg-stone-900 text-stone-100 rounded-md" >
+            <img className="w-4" src="/add-white.svg" alt="add" />
+            Manage Cards
+          </button >
+        </div >
+        <h1 className="text-stone-400">Card 1 of 3</h1>
         <Flashcards />
-        <Controls />
+        <div className='flex gap-5'>
+          <button className='flex items-center px-5 gap-3 h-10 bg-white rounded-md shadow-sm'>
+            <img src="/shuffle.svg" className='w-4' alt="shuffle" />
+            Shuffle
+          </button>
+          <button className='flex items-center px-5 gap-3 h-10 bg-gray-950 rounded-md shadow-sm text-gray-50'>
+            Next
+            <img src="/next.svg" className='w-4' alt="shuffle" />
+          </button>
+        </div>
         <ManageCardsModal />
       </main>
     </div>
